c485b86a0717e72651f4aa21fa604b1a3ce442a8
mihauuu <m.zamelski@wp.pl>
2020-10-18 17:20:04 +0200

    commit

--- lista1/src/lista1/Librarian.java
+++ lista1/src/lista1/Librarian.java
@@ -1,5 +1,35 @@
 package lista1;
 
+
 public class Librarian {
 
+	public static Library library;
+	
+	public Librarian() {
+		
+		library = new Library();
+	}
+	
+	public void lentBook(User user, Book book) {
+		
+		if(library.isBookInStock(book)) {
+			
+			library.removeFromStock(book);
+			user.borrowBook(book);
+		}
+		else
+			System.out.print(book.toString() + "- librarian can't lent this book it's not in stock\n");
+	}
+	
+	public void receiveBook(User user, Book book) {
+		
+		if(user.didUserBorrowBook(book)) {
+			
+			user.returnBook(book);
+			library.addToStock(book);
+		}
+		else
+			System.out.print("User can't return book which he didn't borrow\n");
+	}
+	
 }
